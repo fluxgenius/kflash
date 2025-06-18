aa90db5648150286892c535fe9ac6ee34febb0da
Gedoni Ani <gedoniani@gmail.com>
2025-06-18 10:25:30 +0100

    update

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -45,7 +45,7 @@
               </button>
             </nav>
             <div class="flex items-center space-x-4">
-              <!-- Theme Toggle Button -->
+              <!-- Theme Toggle Button 
               <button @click="toggleTheme" class="text-gray-500 hover:text-indigo-600 dark:text-gray-300 dark:hover:text-indigo-400">
               <svg v-if="theme === 'light'" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 3v1m0 16v1m8.485-8.485h-1M4.515 12h-1m15.364 4.95l-.707-.707M6.343 6.343l-.707-.707m12.728 12.728l-.707-.707M6.343 17.657l-.707-.707M12 5a7 7 0 100 14 7 7 0 000-14z" />
@@ -53,7 +53,7 @@
               <svg v-else xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.293 14.293A8 8 0 1112 4v1a7 7 0 105 9.293z" />
               </svg>
-              </button>
+              </button> -->
               <button @click="mobileMenuOpen = !mobileMenuOpen" class="md:hidden text-gray-500 hover:text-indigo-600 dark:text-gray-300 dark:hover:text-indigo-400">
               <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path v-if="!mobileMenuOpen" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
